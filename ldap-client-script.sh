f8b528c1159676e2a82d53600b34f6fc2965cd41
AmritSSC <43659767+AmritSSC@users.noreply.github.com>
2019-01-31 20:26:21 -0800

    Create ldap-client-script.sh

---
+++ ldap-client-script.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+apt-get update
+apt-get install libnss-ldap libpam-ldap ldap-utils nslcd debconf-utils
+
+
+vim  /etc/nsswitch.conf
+
+vim /etc/ldap/ldap.conf 
+
+#restart nslcd 
+/etc/init.d/nslcd restart
+
+#list all users
+getent passwd
+
+show all users:
+ldapsearch -b "dc=nti310,dc=local" -x -d 1 2>> output.txt 
