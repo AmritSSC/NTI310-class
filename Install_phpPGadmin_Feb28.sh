0ab19950526866ae9a6525c97bac6ee8db8f06cb
AmritSSC <43659767+AmritSSC@users.noreply.github.com>
2019-02-28 20:13:44 -0800

    Update Install_phpPGadmin_Feb28.sh

--- Install_phpPGadmin_Feb28.sh
+++ Install_phpPGadmin_Feb28.sh
@@ -33,6 +33,9 @@ yum install -y phpPgAdmin
 sed -i 's/Require local/Require all granted/g'  /etc/httpd/conf.d/phpPgAdmin.conf
 sed -i 's/Deny from all/Allow from all/g'  /etc/httpd/conf.d/phpPgAdmin.conf
 
+sed -i "s/$conf\['servers'\]\[0\]\['host'\] = '';/$conf\['servers'\]\[0\]\['host'\] = 'localhost';/g" /etc/phpPgAdmin/config.inc.php
+sed -i "s/$conf\['owned_only'\] = false;/$conf\['owned_only'\] = true;/g" /etc/phpPgAdmin/config.inc.php
+
 #restart httpd and postgresql
 systemctl reload httpd.service
 systemctl restart postgresql
