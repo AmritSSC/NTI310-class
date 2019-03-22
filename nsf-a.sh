a6a752a31aa94cd56bc04ca2609a2576bd21937a
AmritSSC <43659767+AmritSSC@users.noreply.github.com>
2019-03-21 18:54:10 -0700

    rsyslog client added

--- nsf-a.sh
+++ nsf-a.sh
@@ -54,3 +54,11 @@ showmount -e $ipaddress # whre $ipaddress is the ip of your nfs server
 ##mount file
 #mount -a
 ##*profit*
+
+#ryslog client automation (install in client server instances):
+yum update -y  && yum install -y rsyslog
+systemctl start rsyslog
+systemctl enable rsyslog
+cp /etc/rsyslog.conf /etc/rsyslog.conf.bak
+
+echo "*.*  @@rsyslog-1:514" >> /etc/rsyslog.conf
